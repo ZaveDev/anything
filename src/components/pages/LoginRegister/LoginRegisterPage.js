406050c79788741eb3a537fa5a57f3a410261d0e
miraclebanks <miraclebnks@gmail.com>
2022-02-08 17:46:20 -0800

    added material ui

--- src/components/pages/LoginRegister/LoginRegisterPage.js
+++ src/components/pages/LoginRegister/LoginRegisterPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import {Outlet} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
 export default function LoginRegisterPage() {
   return (<div>
   <h2>Sign-in</h2>
-    {/* <Outlet/> */}
+    <Outlet/>
   </div>)
 }
--- src/components/pages/LoginRegister/Register.js
+++ src/components/pages/LoginRegister/Register.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import './lr_styles/register.css';
 
 export default function Register() {
-  return <><h2>Register</h2></>;
+  return <>
+    <h2>Register</h2>
+    <div></div>
+  </>;
 }
